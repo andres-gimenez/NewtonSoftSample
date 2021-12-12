7a458b5512fad002154fe59dd11b0e8bfd9a1ff7
Andres Gimenez Muñoz <andres.gimenez@outlook.com>
2021-12-12 16:09:03 +0100

    Add project files.

---
+++ NewtonSoftSample/Program.cs
@@ -0,0 +1,57 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace NewtonSoftSample
+{
+    internal class Program
+    {
+        const string filePath = @".\json.txt";
+
+        public static void Serialize(object obj)
+        {
+            var serializer = new JsonSerializer();
+
+            using (var sw = new StreamWriter(filePath))
+            using (JsonWriter writer = new JsonTextWriter(sw))
+            {
+                serializer.Serialize(writer, obj);
+            }
+        }
+
+        public static object Deserialize(string path)
+        {
+            var serializer = new JsonSerializer();
+
+            using (var sw = new StreamReader(path))
+            using (var reader = new JsonTextReader(sw))
+            {
+                return serializer.Deserialize(reader);
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            var data = new DataStructure
+            {
+                Name = "Henry",
+                Identifiers = new List<int> { 1, 2, 3, 4 }
+            };
+
+            Console.WriteLine("Object before serialization:");
+            Console.WriteLine("----------------------------");
+            Console.WriteLine();
+            Console.WriteLine(data);
+
+            Serialize(data);
+
+            var deserialized = Deserialize(filePath);
+
+            Console.WriteLine("Deserialized (json) string:");
+            Console.WriteLine("---------------------------");
+            Console.WriteLine();
+            Console.WriteLine(deserialized);
+        }
+    }
+}
